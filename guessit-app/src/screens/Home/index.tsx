74490bcf90945972e64f483af6c78980ee04910f
Maxim Dudai <maxim.dudai01@gmail.com>
2025-02-22 18:54:03 +0000

    Guess It

---
+++ guessit-app/src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+    return (
+        <>
+            <div className="container">
+                <h1>Home</h1>
+                <p>Welcome to the Home page</p>
+            </div>
+        </>
+    )
+}
